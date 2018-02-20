f5b2127ac5432109acebbf583002cde76fb1bc02
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2018-02-20 18:54:24 +0400

    Fix

--- app/models/tramway/profiles/social_network.rb
+++ app/models/tramway/profiles/social_network.rb
@@ -2,5 +2,5 @@ class Tramway::Profiles::SocialNetwork < ::Tramway::Core::ApplicationRecord
   belongs_to :record, polymorphic: true
 
   enumerize :network_name, in: [ :vk, :facebook, :twitter ]
-  enumerize :record_type, in: ::Tramway::Profiles.records
+  #enumerize :record_type, in: ::Tramway::Profiles.records
 end
